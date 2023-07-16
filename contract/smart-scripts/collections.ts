4be092c0d6d9f8a6af3e89a501972e573cdeb9f1
Gonzalo Othacehe <gothacehe@spacedev.io>
2023-07-16 19:02:02 -0300

    Successfully obtaining weavedb data trough commander CLI

--- contract/smart-scripts/collections.ts
+++ contract/smart-scripts/collections.ts
@@ -1,9 +1,13 @@
 import { Command } from "commander";
-
-// Import these functions from your existing scripts
+import path from "path";
+import fs from "fs";
+import { weaveDB } from "../weavedb"; // Update the path accordingly
 import { uploadMetadata } from "../scripts/upload-metadata";
 import { generateMerkleTree } from "../scripts/generate-merkle-tree";
 import { deployCollection } from "../scripts/deploy-collection";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const collection = new Command("collection");
 
@@ -64,11 +68,58 @@ collection
     // 2. Generate Merkle Tree
     await generateMerkleTree(brandName, collectionName);
 
-      console.log("\n3. Deploying collection contract...");
+    console.log("\n3. Deploying collection contract...");
     // 3. Deploy Collection
     await deployCollection(brandName, collectionName, symbol, nftCount);
 
+    console.log("\n4. Upload collection to the WeaveDB community");
+
     console.log("Collection created successfully.");
+  });
+
+collection
+  .command("list")
+  .description("List all the collections")
+  .action(() => {
+    console.log("Collections List:");
+    const collectionsPath = path.resolve(
+      __dirname,
+      "..",
+      "deployed-contracts",
+      "deployed-proxy.json"
+    );
+    // Check if the file exists. If it does, read its content, otherwise log an appropriate message
+    if (fs.existsSync(collectionsPath)) {
+      const collectionsData = JSON.parse(fs.readFileSync(collectionsPath, "utf8"));
+      collectionsData.forEach((collection: any, index: number) => {
+        console.log(`\nCollection ${index + 1}:`);
+        console.log("Brand Name:", collection.brandName);
+        console.log("Collection Name:", collection.collectionName);
+        console.log("Units:", collection.units);
+        console.log("Beacon Proxy Address:", collection.beaconProxyAddress);
+        console.log("Deployed With Beacon Address:", collection.deployedWithBeaconAddress);
+        console.log("Chain:", collection.chain);
+        console.log("Deployed at:", collection.time);
+      });
+    } else {
+      console.log("No collections found.");
+    }
+  });
+
+collection
+  .command("list-weavedb")
+  .description("Lists collections from WeaveDB")
+  .action(async () => {
+    console.log("Fetching collections from WeaveDB...");
+    const db = await weaveDB();
+    const collections = await db.cget("communities"); // Assuming 'collection' is the right key
+    console.log("collections", collections);
+    // console.table(
+    //   collections.reduce((acc: any, collection: any) => {
+    //     acc[collection.id] = { name: collection.data.name }; // Update 'name' to correct property if different
+    //     return acc;
+    //   }, {})
+    // );
   });
 
 export default collection;
