56ab835215396062ac53a673c6351dc89e3b1550
Gonzalo Othacehe <gothacehe@spacedev.io>
2023-07-16 21:59:43 -0300

    script cleanup 2

--- contract/smart-scripts/collection.ts
+++ contract/smart-scripts/collection.ts
@@ -63,29 +63,6 @@ collection
       "\nnftCountStr:",
       nftCountStr
     );
-    // Validate communityId
-    if (typeof communityId !== "string" || communityId.length === 0) {
-      console.error("Invalid brand name. Please provide a valid string.");
-      process.exit(1);
-    }
-
-    // Validate collectionName
-    if (typeof collectionName !== "string" || collectionName.length === 0) {
-      console.error("Invalid collection name. Please provide a valid string.");
-      process.exit(1);
-    }
-
-    // Validate description
-    if (typeof description !== "string" || description.length === 0) {
-      console.error("Invalid description. Please provide a valid string.");
-      process.exit(1);
-    }
-
-    // Validate symbol
-    if (typeof symbol !== "string" || symbol.length === 0) {
-      console.error("Invalid symbol. Please provide a valid string.");
-      process.exit(1);
-    }
 
     // Convert string to number
     const nftCount = parseInt(nftCountStr, 10);
@@ -100,12 +77,12 @@ collection
       console.log("\n1. Uploading metadata...");
       await uploadMetadata(communityId, collectionName, description, nftCount);
 
-      console.log("\n2. Generating Merkle Tree from the Merkle values (leafs)...");
       // 2. Generate Merkle Tree
+      console.log("\n2. Generating Merkle Tree from the Merkle values (leafs)...");
       await generateMerkleTree(communityId, collectionName);
 
-      console.log("\n3. Deploying collection contract...");
       // 3. Deploy Collection
+      console.log("\n3. Deploying collection contract...");
       const collectionAddress = await deployCollection(
         communityId,
         collectionName,
@@ -113,8 +90,8 @@ collection
         nftCount
       );
 
-      console.log("\n4. Uploadiing collection to the WeaveDB community...");
       // 4. Upload collection to the WeaveDB community
+      console.log("\n4. Uploadiing collection to the WeaveDB community...");
       await weaveDBCreateCollection(communityId, collectionName, collectionAddress);
     } catch (error) {
       console.log(error);
@@ -130,25 +107,6 @@ collection
   .command("patch <communityId> <collectionName> <collectionAddress>")
   .description("Push created collection into WeaveDB if the collection is not already there")
   .action(async (communityId, collectionName, collectionAddress) => {
-    // Validate communityId
-    if (typeof communityId !== "string" || communityId.length === 0) {
-      console.error("Invalid brand name. Please provide a valid string.");
-      process.exit(1);
-    }
-
-    // Validate collectionName
-    if (typeof collectionName !== "string" || collectionName.length === 0) {
-      console.error("Invalid collection name. Please provide a valid string.");
-
-      process.exit(1);
-    }
-
-    // Validate collectionAddress
-    if (typeof collectionAddress !== "string" || collectionAddress.length === 0) {
-      console.error("Invalid collection address. Please provide a valid string.");
-      process.exit(1);
-    }
-
     try {
       await weaveDBCreateCollection(communityId, collectionName, collectionAddress);
     } catch (error) {
